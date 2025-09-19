aa9a3aa30bb8f6ff56874ebc2185266b65c93e61
jamessch16 <jamessch16@gmail.com>
2025-09-19 10:22:59 -0600

    row search

--- src/a1p1.c
+++ src/a1p1.c
@@ -5,8 +5,13 @@
 #include <unistd.h>
 
 const int NUM_ROWS = 100;
+const int NUM_COLUMNS = 1000;
 
-int created_child_processes(pid_t child_processes[]) {
+
+// TODO TODO What if it is found before the all processes are spawned
+
+
+int create_child_processes(pid_t child_processes[]) {
     /*
     Creates 100 child processes to search the file rows.
     
@@ -23,26 +28,42 @@ int created_child_processes(pid_t child_processes[]) {
 
         pid = fork();
 
+        
+        // TODO TODO What if it is found before the all processes are spawned
+
         if (pid > 0) {
             child_processes[counter] = pid;
             printf("Child %d (PID %d): Searching row %d\n", counter, pid, counter);
+            counter++;
         }
 
-        // wait if process creating failed and reset counter
-        if (pid < 0) {
-            counter--;
-            wait(10);
-        }
-
-        counter++;
+        // wait if process creating failed
+        // if (pid < 0) {
+        //     wait(10);
+        // }
     }
 
     if (pid == 0) return counter;
     else return -1;
 }
 
-void search_row(int row_num) {
+bool search_row(int row_num) {
 
+    char current;
+    bool found = false;
+
+    fseek(stdin, 2 * row_num * NUM_COLUMNS, SEEK_CUR);
+
+    for (int i = 0; i < NUM_COLUMNS && !found; i++) {
+
+        current = getc(stdin);
+
+        if (current == '1') found = true;
+        
+        fseek(stdin, 1, SEEK_CUR);
+    }
+
+    return found;
 }
 
 int main() {
@@ -50,18 +71,31 @@ int main() {
     pid_t child_processes[NUM_ROWS];
     pid_t parent_pid = getpid();
     pid_t pid;
-    bool is_parent = true;
+    int process_row;
+    bool target_found = false;
 
-    is_parent = created_child_processes();
+    process_row = create_child_processes(child_processes);
 
-    // TODO WAIT
+    // process is the parent
+    if (process_row == -1) {
+        wait(NULL); // TODO HERE
 
-    // report PID of process that finds target
-    for (int i = 0; i < NUM_ROWS; i++){
-        if (child_processes[i] = pid) {
-            
+        // report PID of process that finds target
+        for (int i = 0; i < NUM_ROWS; i++) {
+            if (child_processes[i] = pid) {
+
+                // TODO TODO What if it is found before the all processes are spawned
+                
+            }
         }
     }
+    
+    // process is a child
+    else {
+        target_found = search_row(process_row);
+    }
+
+
 
     return 0;
 }
